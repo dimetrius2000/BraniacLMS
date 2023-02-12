28e4b0eef3b94b18523b9f754c58c027d539393d
dimetrius2000 <dimetris311200@mail.ru>
2023-02-12 21:51:40 +0300

    lesson_4 News model

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,7 +6,6 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
     path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
@@ -15,4 +14,3 @@ urlpatterns = [
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
     path("login/", views.LoginPageView.as_view(), name="login"),
 ]
-
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,14 @@
-
 from datetime import datetime
 
 from django.views.generic import TemplateView
-
 
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
 
-
-
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
         # Get all previous data
@@ -30,11 +28,12 @@ class NewsWithPaginatorView(NewsPageView):
         return context
 
 
-
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
 
 
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
 
 
 class DocSitePageView(TemplateView):
@@ -42,6 +41,4 @@ class DocSitePageView(TemplateView):
 
 
 class LoginPageView(TemplateView):
-
     template_name = "mainapp/login.html"
-

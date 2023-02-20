efd4b40a07a8cf5d76ac7a441ad9e9716e2274fe
dimetrius2000 <106590746+dimetrius2000@users.noreply.github.com>
2023-02-20 17:40:09 +0300

    lesson_4 (#4)
    * lesson_4 News model
    
    * lesson_4 Models
    
    * lesson_4 News model
    
    * lesson_4 Models
    
    * lesson_4 Migrations
    
    * lesson_4 Fixtures
    
    * lesson_4 Data migration
    
    * lesson_4 Display model's data

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
     "mainapp",
 ]
 
@@ -124,10 +125,8 @@ STATIC_URL = "/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
